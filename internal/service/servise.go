30a9b3efa10ae70ab221380b748e973f83641365
serediukit <serediukit@gmail.com>
2025-07-22 00:26:58 +0300

    implement service from grpc

---
+++ internal/service/servise.go
@@ -0,0 +1,42 @@
+package service
+
+import (
+	"context"
+	"pickup-srv/internal/repository"
+	"pickup-srv/proto"
+)
+
+type PickupService struct {
+	proto.UnimplementedPickupServiceServer
+
+	userRepo *repository.UserRepository
+}
+
+func NewPickupService(userRepo *repository.UserRepository) *PickupService {
+	return &PickupService{
+		userRepo: userRepo,
+	}
+}
+
+func (s *PickupService) GetUsers(ctx context.Context, req *proto.GetUsersRequest) (*proto.GetUsersResponse, error) {
+	users, err := s.userRepo.GetUsers(req.UserSearchParams, req.Limit)
+	if err != nil {
+		return nil, err
+	}
+
+	protoUsers := make([]*proto.User, len(users))
+	for i, user := range users {
+		protoUsers[i] = &proto.User{
+			Id:    int32(user.ID),
+			Name:  user.Name,
+			Age:   int32(user.Age),
+			City:  user.City,
+			RegDt: user.RegDt.Format("2006-01-02T15:04:05Z"),
+		}
+	}
+
+	return &proto.GetUsersResponse{
+		Users: protoUsers,
+		Total: int32(len(protoUsers)),
+	}, nil
+}
